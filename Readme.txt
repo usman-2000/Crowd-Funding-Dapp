ddbfaad19981a7aaf56d47b22cb28534d24cd349
usmanrahim <usmanrahim2000@gmail.com>
2023-07-13 16:03:17 +0500

    Added Readme for this project

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Crowd Funding Dapp
+
+In this dapp , user can create a project for which he wants to collect the funds . So anyone which has logged in already can give funds to the projects.
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,13 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  return <div>Navbar</div>;
+  return (
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-rowmax-w-[458px] py-2 pl-2 pr-2 h-[52px] bg-[#1c1c24]">
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
